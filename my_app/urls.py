14091f2e2f4a95e92242ce4ccad37885050ddc94
Franguestclain <the_king_00carlos@hotmail.com>
2019-11-12 12:08:58 -0700

    Se arreglo el root de la pagina

--- my_app/urls.py
+++ my_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.home, name='home'),
-	path('new_search', views.new_search, name='new_search'),
-	path('promo', views.promo, name='promo'),
+	path('', views.promo, name='promo'),
+	path('new_search', views.new_search, name='new_search')
+	# path('promo', views.promo, name='promo'),
 ]
