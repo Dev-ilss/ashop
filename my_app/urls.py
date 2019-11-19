0d27ab75874e882c4d673ed46456e04b952a66e5
Franguestclain <the_king_00carlos@hotmail.com>
2019-11-19 10:12:36 -0700

    Se añade nombre de los top en el diccionario

--- my_app/urls.py
+++ my_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.promo, name='promo'),
+	path('', views.topSearch, name='promo'),
 	path('new_search', views.new_search, name='new_search')
 	# path('promo', views.promo, name='promo'),
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -15,6 +15,51 @@ def home(request):
 def promo(request):
 	return render(request, 'my_app/promo.html')
 
+# FIXME: 
+# 	-No llena los campos de guitar y comic
+# 	-Añadir un limite de solo 2 elementos, es decir,2 de cars, 2 de guitar y 2 de comic
+def topSearch(request):
+	final_postings = []
+	j=0
+	search = ['cars', 'guitar', 'comic']
+	for cat in search:
+		final_url = BASE_CRAIGSLIST_URL.format(quote_plus(cat))
+		response = requests.get(final_url)
+		data = response.text
+		soup = BeautifulSoup(data, features='html.parser')
+
+		post_listings = soup.find_all('li', {'class': 'result-row'})
+		final_postings.append({str(cat): []})
+		# TODO:
+		# 	-Limite de solo dos elementos
+		# 	-Tener en cuenta de que no es una lista si no que es un objeto de tipo bs4 (algo asi), ay que convertir
+		# 	-¿Que es el request?
+		# for	post in post_listings:
+		# 	post_titles = post.find(class_='result-title').text
+		# 	post_url = post.find('a').get('href')
+		# 	if post.find(class_='result-price'):
+		# 		post_price = post.find(class_='result-price').text
+		# 	else:
+		# 		post_price = "N/A"
+			
+		# 	if post.find(class_= 'result-image').get('data-ids'):
+		# 		post_image_id = post.find(class_='result-image').get('data-ids').split(',')[0].split(':')[1]
+		# 		post_image_url = BASE_IMAGE_URL.format(post_image_id)
+		# 		print(post_image_url)
+		# 	else:	
+		# 		post_image_url = 'http://maestroselectronics.com/wp-content/uploads/2017/12/No_Image_Available.jpg'
+
+		# 	final_postings[j][cat].append({"titulo":post_titles,"url": post_url,"precio": post_price,"imgurl": post_image_url})
+		j+=1
+	print(type(post_listings))
+	
+
+	stuff_for_frontend = {
+		'search': search,
+		'final_postings': final_postings,
+	}
+	return render(request, 'my_app/promo.html', stuff_for_frontend)
+
 def new_search(request):
 	search = request.POST.get('search')
 	models.Search.objects.create(search=search)
@@ -41,8 +86,9 @@ def new_search(request):
 		else:	
 			post_image_url = 'http://maestroselectronics.com/wp-content/uploads/2017/12/No_Image_Available.jpg'
 
-		final_postings.append((post_titles, post_url, post_price, post_image_url))
+		final_postings.append({"titulo":post_titles,"url": post_url,"precio": post_price,"imgurl": post_image_url})
 
+	# Debug section:
 	# print(post_titles)
 	# print(post_price)
 	# print(post_url)
